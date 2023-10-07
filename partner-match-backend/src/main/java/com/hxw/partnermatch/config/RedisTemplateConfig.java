6fadedafdd324543fce7840264e1d38eee98945f
joycehxw <joycehuangxw@gmail.com>
2023-10-07 17:44:40 +0800

    change serializer

--- partner-match-backend/src/main/java/com/hxw/partnermatch/config/RedisTemplateConfig.java
+++ partner-match-backend/src/main/java/com/hxw/partnermatch/config/RedisTemplateConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
@@ -25,8 +26,14 @@ public class RedisTemplateConfig {
         RedisTemplate<String, Object> redisTemplate=new RedisTemplate<>();
         //设置连接工厂
         redisTemplate.setConnectionFactory(connectionFactory);
-        //设置序列化工具
+        //创建JSON序列化工具
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer=new GenericJackson2JsonRedisSerializer();
+        //设置key的序列化
         redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        //设置value的序列化
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
         return redisTemplate;
     }
 }
